25bd46b4626fcc10f2ba1470c80b12271ca52ac3
Leandro Corrêa <leandro_apcp@hotmail.com>
2017-01-18 20:28:32 -0200

    Início do projeto base

---
+++ Cliente.php
@@ -0,0 +1,15 @@
+<?php
+
+class Cliente {
+
+  public $ficha;
+  public $nome;
+  public $endereco;
+  public $numero;
+  public $bairro;
+  public $cep;
+  public $telefone;
+  public $nascto;
+  public $email;
+
+  }
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Cadastro de Clientes</title>
+  </head>
+  <body>
+    <?php
+    require_once 'Cliente.php';
+    
+     ?>
+  </body>
+</html>
