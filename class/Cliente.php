92a6ca6954f471ec51f8b6d0e5ac02af7f7ad76f
Leandro Corrêa <leandro_apcp@hotmail.com>
2017-01-19 20:56:27 -0200

    Organizando a classe Cliente

--- class/Cliente.php
+++ class/Cliente.php
@@ -14,52 +14,52 @@ class Cliente {
     return $this->nome;
   }
 
-  public function getEndereco(){
-    return $this->endereco;
-  }
-
-  public function getNumero(){
-    return $this->numero;
-  }
-
-  public function getBairro(){
-    return $this->bairro;
-  }
-
-  public function getCep(){
-    return $this->cep;
-  }
-
-  public function getTelefone(){
-    return $this->telefone;
-  }
-
-  public function getEmail(){
-    return $this->email;
-  }
-
   public function setNome($nome){
     $this->nome = $nome;
+  }
+
+  public function getEndereco(){
+    return $this->endereco;
   }
 
   public function setEndereco($endereco){
     $this->endereco = $endereco;
   }
 
+  public function getNumero(){
+    return $this->numero;
+  }
+
   public function setNumero($numero){
     $this->numero = $numero;
+  }
+
+  public function getBairro(){
+    return $this->bairro;
   }
 
   public function setBairro($bairro){
     $this->bairro = $bairro;
   }
 
+  public function getCep(){
+    return $this->cep;
+  }
+
   public function setCep($cep){
     $this->cep = $cep;
   }
 
+  public function getTelefone(){
+    return $this->telefone;
+  }
+
   public function setTelefone($telefone){
     $this->telefone = $telefone;
+  }
+
+  public function getEmail(){
+    return $this->email;
   }
 
   public function setEmail($email){
