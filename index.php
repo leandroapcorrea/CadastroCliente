5d5bf57108e5e81851237701e8524fdafeb4e8a2
Leandro Corrêa <leandro_apcp@hotmail.com>
2017-01-21 13:43:55 -0200

    Finalizando fase 1 projeto

--- index.php
+++ index.php
@@ -1,39 +1,49 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Lista de Clientes</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="css/bootstrap.css" rel="stylesheet" />
-    <link href="css/bootstrap-resposive.css" rel="stylesheet" />
-  </head>
-  <body>
-    <div class="container">
+<head>
+  <meta charset="utf-8">
+  <title>Lista de Clientes</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="css/bootstrap.css" rel="stylesheet" />
+  <link href="css/bootstrap-resposive.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="container">
+    <div class="row">
       <div class="page-header">
         <h1>Lista de Clientes</h1>
       </div>
       <?php
       require_once 'data/meusclientes.php';
-      // print_r($todos_clientes);
+      if(!empty($_GET) && $_GET['order'] == "DESC"){
+        krsort($todos_clientes);
+      }
       ?>
+      <div class="span4 offset8" style="text-align: right; margin-bottom: 20px;">
+        Ordernar por ID:
+        <a href="index.php?order=ASC">Crescente</a>
+        <span>&nbsp;|&nbsp;</span>
+        <a href="index.php?order=DESC">Decrescente</a>
+      </div>
       <table class="table table-bordered table-striped">
         <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Mais</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($todos_clientes as $key => $cliente): ?>
           <tr>
-            <td><?php echo $key; ?></td>
-            <td><b><?php echo $cliente->getNome(); ?></b></td>
-            <td><a href="fichacliente.php?id=<?php echo $key; ?>" class="btn btn-info"><i class="icon-folder-open"></i>&nbsp;Ver mais</a></td>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Mais</th>
           </tr>
-      <?php endforeach; ?>
-    </tbody>
+        </thead>
+        <tbody>
+          <?php foreach ($todos_clientes as $key => $cliente): ?>
+            <tr>
+              <td><?php echo $key; ?></td>
+              <td><b><?php echo $cliente->getNome(); ?></b></td>
+              <td><a href="fichacliente.php?id=<?php echo $key; ?>" class="btn btn-info"><i class="icon-folder-open"></i>&nbsp;Ver dados</a></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
       </table>
+    </div>
   </div>
-  </body>
+</body>
 </html>
